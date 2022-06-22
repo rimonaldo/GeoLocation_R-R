114025f684ee5a4ea8b2ef10a907e29bb063fd64
RoyLeybovitch <roy.leybo@gmail.com>
2022-06-22 19:02:43 +0300

    commit addLocation function

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,32 @@
 export const locService = {
-    getLocs
+    getLocs,
+    addLocation,
 }
 
-
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(locs);
+            resolve(locs)
         }, 2000)
-    });
+    })
 }
 
-
+function addLocation(name, lat, lng) {
+    const loc = {
+        id,
+        name,
+        lat,
+        lng,
+        weather,
+        createdAt,
+        updatedAt: Date.now(),
+    }
+    locs.push(loc)
+    console.log(locs)
+    return locs
+}
